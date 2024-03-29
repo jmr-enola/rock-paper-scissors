1bc6adbf08d0c447db227f8d9f94ebb034cd3a71
jmr-enola <jmr.enola@gmail.com>
2024-03-29 08:39:40 +0800

    modified static page to pass csp policy of helmet

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
+const PORT = 8080;
+
+/** Imports */
 const STATUS = require('./src/utils/httpStatus.js');
 const express = require('express');
 const app = express();
 
-const PORT = 8080;
+//middlewares
+const compression = require("compression");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
+/** End Imports */
+
+app.use(compression()); //compress all routes - https://www.npmjs.com/package/compression
+app.use(helmet({
+    contentSecurityPolicy: {
+      directives: {
+        "script-src": ["'self'","'nonce-pdPtnYIhQcLjAr6'"],
+      },
+    },
+  })); //secure express app by setting HTTP response headers - https://www.npmjs.com/package/helmet
+app.use(rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 100, // 100 request per minute per client(window)
+  })); //limit number of client request - https://www.npmjs.com/package/express-rate-limit
 
 app.use(express.static('static'), express.text());
 
 app.post('/', (req, res, next) => {
+    console.log(req.body);
     const moves = ['rock', 'paper', 'scissors'];
 
     const gameResult = (move) => {
